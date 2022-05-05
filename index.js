54932d513590bdd7985c96cc990258141ed8aa19
tanianasrin1 <tasrineva169@gmail.com>
2022-05-05 17:44:59 +0600

    inventory item

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require ('express');
 const app = express();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors');
+const res = require('express/lib/response');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
@@ -16,10 +17,27 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 
 async function run(){
     try{
+        await client.connect();
+        const serviceCollection = client.db('oilService').collection('inventory');
+        
+        app.get('/inventory', async(req, res)=> {
+            const query = {};
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services)
+        });
+
+        app.get('/inventory/:id', async(req, res) => {
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const service = await serviceCollection.findOne(query);
+            res.send(service);
+        })
+        
 
     }
     finally{
-        
+
     }
 
 }
@@ -32,5 +50,5 @@ app.get('/', (req, res) => {
 });
 
 app.listen(port, ()=> {
-    console.log('Server is running')
+    console.log('Server is running', port)
 })
